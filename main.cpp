e320d22d8c3ba8f2d63e6232da580af9e8ec53e4
kjan2 <kjan@test.com>
2022-06-10 18:31:58 +0300

    wow

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
 
-    BinarySearchTree<> tree;
+    Map<int, int> tree;
     tree.insert(15, 3);
     tree.insert(6, 3);
     tree.insert(3, 3);
@@ -17,21 +17,7 @@ int main()
     tree.insert(20, 4);
     tree.insert(3, 4);
 
-    tree.print(tree._root);
-    
-    //tree.deleteNode(tree._root);
-    BinarySearchTree<>::Iterator it(tree._root);
-    std::cout << tree.size() << std::endl;
-    BinarySearchTree<>::Iterator it2(nullptr);
 
-    tree.print(tree._root);
-
-    for(auto& i : tree)
-    {
-        std::cout << (i).first << " " << (i).second << std::endl;
-    } 
-    
-    tree.equalRange(0);
 
     return 0;
 }
